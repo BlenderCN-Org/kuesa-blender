5651af48ee3394a954e0b24edaa49c19d501d2ad
Paul Lemire <paul.lemire@kdab.com>
2019-02-28 07:20:39 +0100

    Add shell script to automatically generate zip package for the Blender modules
    Task-Id: KUE-589

---
+++ generate_package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASE_PWD=$(dirname "$(readlink -f "$0")")
+KUESA_TOOLS_DIR=$BASE_PWD/addons/
+KUESA_EXPORTER_DIR=$BASE_PWD/addons/kuesa_exporter/glTF-Blender-Exporter/scripts/addons/
+DATE_STR=$(date "+%d_%m_%y")
+GIT_VERSION=$(git describe --tags)
+
+# Create zip file for Kuesa Tools
+cd "$KUESA_TOOLS_DIR" && zip -r "$BASE_PWD/kuesa_${DATE_STR}_${GIT_VERSION}.zip" kuesa
+
+# Create zip file for Kuesa Exporter
+cd "$KUESA_EXPORTER_DIR" && zip -r "$BASE_PWD/kuesa_exporter_${DATE_STR}_${GIT_VERSION}.zip" io_scene_gltf2
