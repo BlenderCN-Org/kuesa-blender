26541de17d031130d2ba7f86ecbe861063ad92cb
Paul Lemire <paul.lemire@kdab.com>
2019-04-25 11:05:07 +0200

    Update package generator to generate both 2.79 and 2.80 packages

--- generate_package.sh
+++ generate_package.sh
@@ -2,12 +2,16 @@
 
 BASE_PWD=$(dirname "$(readlink -f "$0")")
 KUESA_TOOLS_DIR=$BASE_PWD/addons/
-KUESA_EXPORTER_DIR=$BASE_PWD/addons/kuesa_exporter/2.79/glTF-Blender-Exporter/scripts/addons/
+KUESA_EXPORTER_DIR_2_79=$BASE_PWD/addons/kuesa_exporter/2.79/glTF-Blender-Exporter/scripts/addons/
+KUESA_EXPORTER_DIR_2_80=$BASE_PWD/addons/kuesa_exporter/2.80/glTF-Blender-IO-master/addons/
 DATE_STR=$(date "+%d_%m_%y")
 GIT_VERSION=$(git describe --tags)
 
 # Create zip file for Kuesa Tools
 cd "$KUESA_TOOLS_DIR" && zip -r "$BASE_PWD/kuesa_${DATE_STR}_${GIT_VERSION}.zip" kuesa
 
-# Create zip file for Kuesa Exporter
-cd "$KUESA_EXPORTER_DIR" && zip -r "$BASE_PWD/kuesa_exporter_${DATE_STR}_${GIT_VERSION}.zip" io_scene_gltf2
+# Create zip file for Kuesa Exporter 2.79
+cd "$KUESA_EXPORTER_DIR_2_79" && zip -r "$BASE_PWD/kuesa_exporter_2.79_${DATE_STR}_${GIT_VERSION}.zip" io_scene_gltf2
+
+# Create zip file for Kuesa Exporter 2.80
+cd "$KUESA_EXPORTER_DIR_2_80" && zip -r "$BASE_PWD/kuesa_exporter_2.80_${DATE_STR}_${GIT_VERSION}.zip" io_scene_gltf2
